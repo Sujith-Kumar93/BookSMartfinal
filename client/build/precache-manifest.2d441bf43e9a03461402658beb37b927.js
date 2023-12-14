af3ff1e90f5531ce97ea2ccd55bcb0906d41352f
Sujith-Kumar93 <121957345+Sujith-Kumar93@users.noreply.github.com>
2023-12-14 19:16:23 +0530

    Add files via upload

---
+++ client/build/precache-manifest.2d441bf43e9a03461402658beb37b927.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0d2bd1b56d921a5e5798b952bb76b4bb",
+    "url": "/index.html"
+  },
+  {
+    "revision": "dff02693ba4a8419173d",
+    "url": "/static/css/main.496e0d97.chunk.css"
+  },
+  {
+    "revision": "f0f5515c66be942f6d26",
+    "url": "/static/js/2.6899dbf4.chunk.js"
+  },
+  {
+    "revision": "dff02693ba4a8419173d",
+    "url": "/static/js/main.96296aa0.chunk.js"
+  },
+  {
+    "revision": "c20bda0da91af47bb8f6",
+    "url": "/static/js/runtime-main.8fc0d6a4.js"
+  }
+]);
---
+++ client/src/core/About.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import Layout from '../core/Layout';
+
+const Home = () => {
+    return (
+        <Layout title="About" description="">
+                      <p>At BOOKSMART, we believe in the transformative power of books. In a world that's constantly evolving, we're on a mission to make the magic of literature accessible to everyone, anytime, anywhere.We are committed to fostering a love for literature and promoting a culture of continuous learning.Thank you for being a part of the BOOKSMART journey.</p>
+        </Layout>
+           
+    )
+};
+
+export default Home
